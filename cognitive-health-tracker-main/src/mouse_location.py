b62afbc0f73b34787ff2c5694e7f2c46145a3fb0
Kazi <rafeekazi00@gmail.com>
2023-03-31 16:19:57 -0400

    making an edit

--- cognitive-health-tracker-main/src/mouse_location.py
+++ cognitive-health-tracker-main/src/mouse_location.py
@@ -1,8 +1,12 @@
 import csv
 import pandas as pd
+import os
+import sys
 
 def mouse_function():
     print("Mouse location during Test")
-    print(pyautogui.position())
-    #time.sleep(1)
+    print(os.path.join(sys.path[0], "my_file.txt"))
+    #print(pyautogui.position())
     '''Time.sleep makes the whole test glitch'''
+
+mouse_function()
