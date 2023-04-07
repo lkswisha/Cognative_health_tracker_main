add28c02ab297a4ce791b73d82777691a6e2366c
Kazi <rafeekazi00@gmail.com>
2023-04-07 15:50:38 -0400

    making an edit

--- cognitive-health-tracker-main/src/mouse_location.py
+++ cognitive-health-tracker-main/src/mouse_location.py
@@ -3,10 +3,12 @@ import pandas as pd
 import os
 import sys
 from datetime import datetime
+import pyautogui
 
 def mouse_location():
     mousedata = [datetime.now().strftime("%H_%M_%S"),pyautogui.position()]
     a=datetime.now().strftime("%d_%m_%Y_%H_%M_%S") + ".csv"
+    pd.read_csv(os.path.join(sys.path[0], "heatMap", a ))
     with open( a, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(mousedata)
